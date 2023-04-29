3917a43b78fbd1d972364e5e90e96bfb4eb338f9
Alyona <borisovsk_a@mail.ru>
2023-04-29 23:41:10 +0300

    feat: Add interceptor

--- app/src/main/java/com/example/besthack/network/BestHackApi.kt
+++ app/src/main/java/com/example/besthack/network/BestHackApi.kt
@@ -1,7 +1,10 @@
 package com.example.besthack.network
 
+import android.util.Log
 import com.example.besthack.ui.theme.models.City
+import okhttp3.Interceptor
 import okhttp3.OkHttpClient
+import okhttp3.Response
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
@@ -19,11 +22,31 @@ interface BestHackApi {
         private const val BASE_URL = "https://644d4a6957f12a1d3ddc4d6e.mockapi.io/"
 
         private fun getRetrofit(): Retrofit {
-            val logging = HttpLoggingInterceptor()
-            logging.setLevel(HttpLoggingInterceptor.Level.BODY)
+            val logging = Interceptor { chain ->
+                val request = chain.request()
+                lateinit var response: Response
+                var responseOK: Boolean = false
+                var tryCount: Int = 0
+
+                while (!responseOK && tryCount < 3) {
+                    try {
+                        response = chain.proceed(request);
+                        responseOK = response.isSuccessful;
+                    }catch (e: Exception){
+                        Log.d("TAG", "Request is not successful - $tryCount");
+                    }finally{
+                        tryCount++
+                    }
+                }
+
+                // otherwise just pass the original response on
+                response
+            }
+
             val okHttp = OkHttpClient.Builder()
                 .addInterceptor(logging)
                 .build()
+
             return Retrofit.Builder()
                 .baseUrl(BASE_URL)
                 .addConverterFactory(GsonConverterFactory.create())
