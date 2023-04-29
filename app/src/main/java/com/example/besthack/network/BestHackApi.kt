c3db023ae7c340f6c289a6c046998fbf55dcddcc
Alyona <borisovsk_a@mail.ru>
2023-04-29 23:25:52 +0300

    feat: Add interceptor

--- app/src/main/java/com/example/besthack/network/BestHackApi.kt
+++ app/src/main/java/com/example/besthack/network/BestHackApi.kt
@@ -1,6 +1,8 @@
 package com.example.besthack.network
 
 import com.example.besthack.ui.theme.models.City
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.create
@@ -17,9 +19,15 @@ interface BestHackApi {
         private const val BASE_URL = "https://644d4a6957f12a1d3ddc4d6e.mockapi.io/"
 
         private fun getRetrofit(): Retrofit {
+            val logging = HttpLoggingInterceptor()
+            logging.setLevel(HttpLoggingInterceptor.Level.BODY)
+            val okHttp = OkHttpClient.Builder()
+                .addInterceptor(logging)
+                .build()
             return Retrofit.Builder()
                 .baseUrl(BASE_URL)
                 .addConverterFactory(GsonConverterFactory.create())
+                .client(okHttp)
                 .build()
         }
         val api: BestHackApi = getRetrofit().create()
