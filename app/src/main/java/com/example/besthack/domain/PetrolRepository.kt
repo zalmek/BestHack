09439e85eff052a9a84663da1735fda3301594af
Alyona <borisovsk_a@mail.ru>
2023-04-29 17:28:22 +0300

    feat: add Repo and VM

---
+++ app/src/main/java/com/example/besthack/domain/PetrolRepository.kt
@@ -0,0 +1,8 @@
+package com.example.besthack.domain
+
+import com.example.besthack.models.PetrolCityCourse
+import kotlinx.coroutines.flow.Flow
+
+interface PetrolRepository {
+    fun getPetrolCityCourse(city: String, dateStart: String, dateEnd: String): Flow<PetrolCityCourse>
+}
---
+++ app/src/main/java/com/example/besthack/models/PetrolCityCourse.kt
@@ -0,0 +1,3 @@
+package com.example.besthack.models
+
+data class PetrolCityCourse(val city: String, val petrolPeriodCourses: List<PetrolPeriodCourse>)
