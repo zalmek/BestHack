196b8f09af484c9a924f213c37ddddeac24c4e61
Alyona <borisovsk_a@mail.ru>
2023-04-29 18:53:58 +0300

    feat: Add city list

--- app/src/main/java/com/example/besthack/domain/PetrolRepository.kt
+++ app/src/main/java/com/example/besthack/domain/PetrolRepository.kt
@@ -5,4 +5,5 @@ import kotlinx.coroutines.flow.Flow
 
 interface PetrolRepository {
     fun getPetrolCityCourse(city: String, dateStart: String, dateEnd: String): Flow<PetrolCityCourse>
+    fun getCities(): Flow<List<String>>
 }
